90ef72532365ca26560c00653cd69b781e475b12
Jaimelen10 <jleonmulero@gmail.com>
2024-04-28 19:12:38 +0200

    Main Actualizado

--- CineJaimeGerman/src/main/kotlin/Main.kt
+++ CineJaimeGerman/src/main/kotlin/Main.kt
@@ -1,5 +1,33 @@
 package org.example
 
+import org.example.di.*
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.fileProperties
+import org.koin.test.check.checkKoinModules
+
+/**
+ * funcion main para ejecutar el programa al completo
+ * @author Jaime Leon
+ * @since 1.0.0
+ */
 fun main() {
-    println("Hello World!")
+    checkMyModules()
+
+    startKoin {
+        printLogger()
+        fileProperties("/config.properties")
+        modules(listOf(databaseModule, clienteModule, ventaModule, productoModule))
+    }
+
+    val app = CineJaimeGermanApp()
+    app.run()
+}
+
+/**
+ * funcion para comprobar la implementacion de los modulos de koin
+ * @author Jaime Leon
+ * @since 1.0.0
+ */
+fun checkMyModules() {
+    checkKoinModules(listOf(databaseModule, clienteModule, ventaModule, productoModule))
 }
